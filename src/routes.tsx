a9bc3df49cd289d2a1d82b4f4278adde2e568814
tennis <kumarteneesh@gmail.com>
2025-02-14 20:38:46 +0530

    feat: add routes for TopicAnalyticsPage and UrlAnalyticsPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,8 @@ import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import AuthSuccess from "./pages/AuthSuccess";
 import AnalyticsPage from "./pages/Analytics";
+import { TopicAnalyticsPage, UrlAnalyticsPage } from "./pages/UrlAnalytics";
+
 
 const AppRoutes = () => {
   return (
@@ -13,6 +15,8 @@ const AppRoutes = () => {
         <Route path="/auth-success" element={<AuthSuccess />}/>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/analytics/topic" element={<TopicAnalyticsPage />} /> 
+        <Route path="/analytics/url" element={<UrlAnalyticsPage />} /> 
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
